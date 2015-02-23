ba0313172e1de680e22f5d27024f86575c2fa98e
plab-idi <terjero@idi.ntnu.no>
2015-02-23 17:30:26 +0100

    Forelesning 7

--- Forelesning_7_Robotkontroll/BT_Robot_Remote_Control/BT_Arduino_RobotRemoteControl.ino
+++ Forelesning_7_Robotkontroll/BT_Robot_Remote_Control/BT_Arduino_RobotRemoteControl.ino
@@ -7,7 +7,7 @@
 const int btUnitTxPin = 2; // Connected to tx on bt unit
 const int btUnitRxPin = 6; // Connected to rx on bt unit
 PLabBTSerial btSerial(btUnitTxPin, btUnitRxPin);
-ZumoMotors motors;
+// ZumoMotors motors;
 PLab_ZumoMotors PLab_motors;
 
 void setup(void)
@@ -29,29 +29,22 @@ void loop()
 
 void readCommand (char *text) {
   if (0 == strcmp("FORWARD", text)) {
-    motors.setSpeeds(200, 200);
+    PLab_motors.setSpeeds(200, 200);
   } else if (0 == strcmp("STOP", text)) {
-    motors.setSpeeds(0, 0);
+    PLab_motors.setSpeeds(0, 0);
   } else if (0 == strcmp("BACKWARD", text)) {
-    motors.setSpeeds(-200, -200);
+    PLab_motors.setSpeeds(-200, -200);
   } else if (0 == strcmp("LEFT", text)) {
-    motors.setSpeeds(0, 0);
     PLab_motors.turnLeft(200, 90);
   } else if (0 == strcmp("L45", text)) {
-   motors.setSpeeds(0, 0);
    PLab_motors.turnLeft(200, 45);
   } else if (0 == strcmp("RIGHT", text)) {
-    motors.setSpeeds(0, 0);
     PLab_motors.turnRight(200, 90);
   } else if (0 == strcmp("R45", text)) {
-   motors.setSpeeds(0, 0);
    PLab_motors.turnRight(200, 45);  
   } else if (0 == strcmp("STEPF", text)) {
-    motors.setSpeeds(0, 0);
     PLab_motors.forward(200, 5);
   } else if (0 == strcmp("STEPB", text)) {
-   motors.setSpeeds(0, 0);
    PLab_motors.backward(200, 5);  
-   
   } 
 }
